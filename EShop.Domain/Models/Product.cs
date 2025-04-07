3910af8cbe9a3f2529cc1dd84a3c02a80c8c03ba
KarolinaGorska <231712@student.uek.krakow.pl>
2025-04-07 02:20:12 +0200

    Add project files.

---
+++ EShop.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using EShopService.Models;
+using System.ComponentModel;
+
+namespace EShop.Domain.Models
+{
+    public class Product : BaseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Ean { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Sku { get; set; }
+        public Category Category { get; set; }
+    }
+}
